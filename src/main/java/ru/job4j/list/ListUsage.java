87c83a7e6ea3068b7b4ae6822f60ca71819252a0
Artem Dagridjan <ob11to61@yandex.ru>
2025-05-07 16:59:03 +0300

    fix checkstyle

--- src/main/java/ru/job4j/list/ListUsage.java
+++ src/main/java/ru/job4j/list/ListUsage.java
@@ -6,19 +6,16 @@ import java.util.List;
 
 public class ListUsage {
     public static void main(String[] args) {
-/*
-        asList();
-        listOf();
-*/
-        List<String> result = new ArrayList<>(); // В ОБЫЧНОМ СПИСКЕ МОЖНО ВСЁ ДЕЛАТЬ
+        List<String> result = new ArrayList<>();
+
         result.add("one");
         result.add("two");
         result.add("three");
         result.remove(0);
 
-        result.replaceAll(n -> n + 1); // Заменяет каждый элемент списка на результат применения оператора
+        result.replaceAll(n -> n + 1);
 
-        result.sort(String::compareTo); // Передать Компаратор
+        result.sort(String::compareTo);
 
         for (String string : result) {
             System.out.println("Текущий элемент: " + string);
@@ -26,16 +23,16 @@ public class ListUsage {
     }
 
     private static void listOf() {
-        List<String> listOf = List.of("a", "b", "c"); // СПИСОК ПОЛНОСТЬЮ НЕЛЬЗЯ МОДИФИЦИРОВАТЬ И МЕНЯТЬ !!! UnsupportedOperationException
-        listOf.set(0, "e"); // НЕЛЬЗЯ
-        listOf.add("d"); // НЕЛЬЗЯ
-        listOf.remove("a"); // НЕЛЬЗЯ
+        List<String> listOf = List.of("a", "b", "c");
+        listOf.set(0, "e");
+        listOf.add("d");
+        listOf.remove("a");
     }
 
     private static void asList() {
-        List<String> list = Arrays.asList("a", "b", "c"); // В ЭТОМ СПИСКЕ НЕЛЬЗЯ ДОБАВИТЬ ИЛИ УДАЛИТЬ ЭЛЕМЕНТЫ, будет .UnsupportedOperationException
-        list.remove(0); //НЕЛЬЗЯ
-        list.add("d"); // НЕЛЬЗЯ
-        list.set(0, "d"); // НО МОДИФИЦИРОВАТЬ МОЖНО !!!!!!!!!
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.remove(0);
+        list.add("d");
+        list.set(0, "d");
     }
 }
