63941d13705e4f6960a888cd49e05f4bbd60563e
ob11to <ob11to61@yandex.ru>
2025-08-29 15:45:18 +0300

    3. Longest Substring Without Repeating Characters
    Rang -> Medium
    
    Topics -> Hash Table
              String
              Sliding Window
    
    https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=problem-list-v2&envId=hash-table

---
+++ src/main/java/ru/job4j/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,30 @@
+package ru.job4j.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abba"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                left = map.get(c) + 1;
+            }
+
+            map.put(c, right);
+            maxLen = right - left + 1;
+        }
+
+        return maxLen;
+    }
+}
