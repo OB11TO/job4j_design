58094fa592483028ba3e5ec98d97c2b6b749ef44
ob11to <ob11to61@yandex.ru>
2025-09-02 14:41:34 +0300

    344. Reverse String
    Rang -> Easy
    
    Topics -> Two Pointers
              String
    
    https://leetcode.com/problems/reverse-string/description/?envType=problem-list-v2&envId=two-pointers

---
+++ src/main/java/ru/job4j/leetcode/two_pointer/ReverseString.java
@@ -0,0 +1,20 @@
+package ru.job4j.leetcode.two_pointer;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left <= right) {
+
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+
+            right--;
+            left++;
+
+        }
+    }
+}
