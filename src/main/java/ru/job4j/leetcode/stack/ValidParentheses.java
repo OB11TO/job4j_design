c5fa7d86c1d7ad4cffb7ebd5d8bc0620afedc242
ob11to <ob11to61@yandex.ru>
2025-09-23 13:33:56 +0300

    20. Valid Parentheses
    Rang -> Easy
    
    Topics -> String
              Stack
    
    https://leetcode.com/problems/valid-parentheses/

---
+++ src/main/java/ru/job4j/leetcode/stack/ValidParentheses.java
@@ -0,0 +1,25 @@
+package ru.job4j.leetcode.stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s.length() <= 1) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c =='(') {
+                stack.push(')');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
